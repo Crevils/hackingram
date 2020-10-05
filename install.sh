995e19b541c6fdec75d1f621ad914ac7433d7557
Crevil <63445750+Crevils@users.noreply.github.com>
2020-10-05 19:48:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Author: github.com/crevils
+#Instagram: @HackZec
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
